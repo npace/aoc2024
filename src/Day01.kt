f45cfddfc26611a01874012900d32785fe562201
Lubomir Naydenov <lubomir.naydenov.ext@jobandtalent.com>
2024-12-03 08:50:41 +0200

    Solve day 1 - both parts

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,49 @@
+import kotlin.math.absoluteValue
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val (sortedLeft, sortedRight) = parseAndSortInput(input)
+
+        var totalDistance = 0
+        sortedLeft.forEachIndexed { index, left ->
+            val right = sortedRight[index]
+            val distance = (left - right).absoluteValue
+            totalDistance += distance
+        }
+        println("Total distance: $totalDistance")
+        return totalDistance
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val (sortedLeft, sortedRight) = parseAndSortInput(input)
+
+        val countMap = mutableMapOf<Int, Int>()
+        sortedRight.forEach { right ->
+            val countForRight = countMap.getOrPut(right) { 0 }
+            countMap[right] = countForRight + 1
+        }
+
+        val similarityScore = sortedLeft.sumOf { it * countMap.getOrDefault(it, 0) }
+        return similarityScore
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    part1(testInput) shouldBe 11
+    part2(testInput) shouldBe 31
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
+}
+
+private fun parseAndSortInput(input: List<String>): Pair<List<Int>, List<Int>> {
+    val (listLeft, listRight) = input.map {
+        val first = it.substringBefore(" ").toInt()
+        val second = it.substringAfterLast(" ").toInt()
+        first to second
+    }.unzip()
+    return listLeft.sorted() to listRight.sorted()
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -19,3 +19,9 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+infix fun Int.shouldBe(expected: Int) {
+    if (this != expected) {
+        error("Expected $expected but got $this")
+    }
+}
